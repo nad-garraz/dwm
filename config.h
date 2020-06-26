29a9707e31da98985ce26752d7cb322725730226
nad-garraz <nad.neutron@pm.me>
2020-06-27 08:40:56 +1200

    ...

--- config.h
+++ config.h
@@ -15,9 +15,9 @@ static const char *fonts[]          = { "monospace:size=12:antialias=true" };
 static const char dmenufont[]       = "monospace:size=12:antialias=true";
 static const char col_gray1[]       = "#000000";
 static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray3[]       = "#FFFFFF";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#555577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
