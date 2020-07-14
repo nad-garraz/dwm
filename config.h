ebcbccf5e7aa4fe1e5ccbb89c20a15506a80404e
nad-garraz <nad.neutron@pm.me>
2020-07-14 18:37:57 +1200

    added mpvwebcam rule

--- config.h
+++ config.h
@@ -41,6 +41,7 @@ static const Rule rules[] = {
 	{ "mpv",     	NULL,     NULL,           0,         	1,          	0,              -1 },
 	{ "sxiv",      	NULL,     NULL,           0,         	0,          	0,              -1 },
 	{ "mpv",  	NULL,     "mpvfloat",     0,         	1,          	1,              -1 },
+	{ "mpv",  	NULL,     "mpvwebcam",    0,         	0,          	1,              -1 },
 	{ NULL,      	NULL,     "Event Tester", 0,        	0,		1,          	-1 }, /* xev */
 };
 
