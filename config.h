4e0893233e0f4b0a3e05fcc09e3c194df05beceb
nad-garraz <nad.neutron@pm.me>
2020-07-20 18:34:54 +1200

    rules

--- config.h
+++ config.h
@@ -1,12 +1,12 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 10;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 30;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 30;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 30;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 25;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 25;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 25;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 25;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -37,9 +37,7 @@ static const Rule rules[] = {
 	{ "Gimp",    	NULL,     NULL,           0,        	0,		1,          	-1 },
 	{ "Firefox", 	NULL,     NULL,           1 << 8,   	0,		0,          	-1 },
 	{ "Alacritty",  NULL,     NULL,           0,         	0,          	0,              -1 },
-	{ "term",      	"float",  NULL,           0,         	1,          	1,              -1 },
-	{ "mpv",     	NULL,     NULL,           0,         	1,          	0,              -1 },
-	{ "sxiv",      	NULL,     NULL,           0,         	0,          	0,              -1 },
+	{ "Alacritty",  "float",  NULL,           0,         	1,          	1,              -1 },
 	{ "mpv",  	NULL,     "mpvfloat",     0,         	1,          	1,              -1 },
 	{ "mpv",  	NULL,     "mpvwebcam",    0,         	0,          	1,              -1 },
 	{ NULL,      	NULL,     "Event Tester", 0,        	0,		1,          	-1 }, /* xev */
